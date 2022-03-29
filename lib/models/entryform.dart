a6dcdc080bb62e8205b9c55f370cba550238ddfd
KevinBenaya <darkfaust063@gmail.com>
2022-03-29 10:51:03 +0700

    Adding name and price

--- lib/models/entryform.dart
+++ lib/models/entryform.dart
@@ -22,6 +22,56 @@ class _EntryFormState extends State<EntryForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    if (item != null) {
+      nameController.text = item.name;
+      priceController.text = item.price.toString();
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: item == null ? Text('Tambah') : Text('Ubah'),
+        leading: Icon(Icons.keyboard_arrow_left),
+      ),
+      body: Padding(
+        padding: EdgeInsets.only(top: 15.0, left: 10.0, right: 10.0),
+        child: ListView(
+          children: <Widget>[
+            //nama barang
+            Padding(
+              padding: EdgeInsets.only(top:20.0, bottom: 20.0),
+              child: TextField(
+                controller: nameController,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: 'Nama Barang',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                onChanged: (value){
+
+                },
+              ),
+            ),
+            //harga barang 
+            Padding(
+              padding: EdgeInsets.only(top:20.0, bottom: 20.0),
+              child: TextField(
+                controller: nameController,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: 'Nama Barang',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                onChanged: (value){
+
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    )
   }
 }
