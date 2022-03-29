7bf8163e442ea0dddc4cb9075884aaa6c199a528
KevinBenaya <darkfaust063@gmail.com>
2022-03-29 09:51:33 +0700

    add setter and getter

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -2,4 +2,15 @@ class Item {
   int _id;
   String _name;
   int _price;
+
+  int get id => _id;
+
+  String get name => this._name;
+  set name(String value) => this._name = value;
+
+  get price => this._price;
+  set price(value) => this._price = value;
+
+  // konstruktor versi 1
+  Item(this._name, this._price);
 }
